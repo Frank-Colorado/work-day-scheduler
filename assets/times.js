851f0a921e572d0b6a175b961efcb4c6359b6af9
Frank-Colorado <chawnkiee@gmail.com>
2023-05-23 21:21:43 -0400

    added content to times.js file

---
+++ assets/times.js
@@ -0,0 +1,38 @@
+const timeBlocks = [
+  {
+    time: 9,
+    header: "9AM",
+  },
+  {
+    time: 10,
+    header: "10AM",
+  },
+  {
+    time: 11,
+    header: "11AM",
+  },
+  {
+    time: 12,
+    header: "12PM",
+  },
+  {
+    time: 13,
+    header: "1PM",
+  },
+  {
+    time: 14,
+    header: "2PM",
+  },
+  {
+    time: 15,
+    header: "3PM",
+  },
+  {
+    time: 16,
+    header: "4PM",
+  },
+  {
+    time: 17,
+    header: "5PM",
+  },
+];
