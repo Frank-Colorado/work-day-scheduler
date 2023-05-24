d48ca9bb915fa5e78b3397b596718b0caf1c819d
Frank-Colorado <chawnkiee@gmail.com>
2023-05-23 21:21:23 -0400

    created times.js file

--- assets/script.js
+++ assets/script.js
@@ -2,45 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-const timeBlocks = [
-  {
-    time: 9,
-    header: "9AM",
-  },
-  {
-    time: 10,
-    header: "10AM",
-  },
-  {
-    time: 11,
-    header: "11AM",
-  },
-  {
-    time: 12,
-    header: "12PM",
-  },
-  {
-    time: 13,
-    header: "1PM",
-  },
-  {
-    time: 14,
-    header: "2PM",
-  },
-  {
-    time: 15,
-    header: "3PM",
-  },
-  {
-    time: 16,
-    header: "4PM",
-  },
-  {
-    time: 17,
-    header: "5PM",
-  },
-];
-
 // This is a function that will load the saved data from local storage
 const loadSavedData = () => {
   const todos = JSON.parse(localStorage.getItem("todos")) || [];
