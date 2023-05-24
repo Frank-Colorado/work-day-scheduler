1efc7cdc01e09038bf0f147f82d85b29ea5247a0
Frank-Colorado <chawnkiee@gmail.com>
2023-05-23 20:13:24 -0400

    created currentDayDisplay function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+const currentDayDisplay = () => {
+  let today = dayjs();
+  // displays the date and day of the week in the header
+  $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
+};
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
